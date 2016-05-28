684ac822432dfb4459f4d0127f349cc7defcbd5e
Obi <obi.netbits@outlook.com>
2016-05-28 12:51:18 +0000

    add loop rendered html

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
+app.get("/posts", function(req, res){
+    var posts =[
+        {tittle: "Never forget", author: "Susy"},
+        {tittle: "My cute car", author: "Benny"},
+        {tittle: "Where I live", author: "Heiskey"}
+        ];
+        
+        res.render("posts.ejs", {posts: posts})
+});
+
 //fallinlovewith/rusty
 app.get ("/fallinginlovewith/:thing", function(req, res){
     var thing = req.params.thing;
