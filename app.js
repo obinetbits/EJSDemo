33932b6926317a1e4afeb507640a6d7fa544ec2b
Obi <obi.netbits@outlook.com>
2016-05-28 13:30:58 +0000

    add partials for header and footer

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/posts", function(req, res){
@@ -12,13 +15,13 @@ app.get("/posts", function(req, res){
         {tittle: "Where I live", author: "Heiskey"}
         ];
         
-        res.render("posts.ejs", {posts: posts})
+        res.render("posts", {posts: posts})
 });
 
 //fallinlovewith/rusty
 app.get ("/fallinginlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 
